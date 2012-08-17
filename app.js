9e2016bfa07247d2943b557414cde4e8afc1f492
Aaron Harpole <aharpole@gmail.com>
2012-08-16 21:56:58 -0700

    using a more correct Twitter URL to post the tweet.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 http.createServer(function(request,response) {
 	var referrer = request.headers['referer'];
 	response.writeHead(302, {
-		'Location': 'http://twitter.com/home?status=Check%20Out%This%20Video%3A%20' + referrer
+		'Location': 'http://twitter.com/intent/tweet?text=Check%20Out%This%20Video&url=' + encodeURIComponent(referrer)
 	});
 	response.end();
 }).listen(port);
