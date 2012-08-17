2966b9d31089e35ebb40d2b8eab496dd126ad0a0
Aaron Harpole <aharpole@gmail.com>
2012-08-16 21:40:48 -0700

    now it toots to Twitter!

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var port = process.env.PORT || 5000;
 http.createServer(function(request,response) {
-	response.writeHead(200);
-	response.write("Hello, this is dog.");
+	var referrer = request.headers['referer'];
+	response.writeHead(302, {
+		'Location': 'http://twitter.com/homt?status=Check%20Out%This%20Video%3A%20' + referrer
+	});
 	response.end();
 }).listen(port);
