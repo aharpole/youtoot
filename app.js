8f2947f3aee0b71108f6ec1b2b562ad222429626
Aaron Harpole <aharpole@gmail.com>
2012-08-16 22:24:06 -0700

    adding a 20 to make that URL actually work.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 http.createServer(function(request,response) {
 	var referrer = request.headers['referer'];
 	response.writeHead(302, {
-		'Location': 'http://twitter.com/intent/tweet?text=Check%20Out%This%20Video&url=' + encodeURIComponent(referrer)
+		'Location': 'http://twitter.com/intent/tweet?text=Check%20Out%20This%20Video&url=' + encodeURIComponent(referrer)
 	});
 	response.end();
 }).listen(port);
